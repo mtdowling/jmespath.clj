cc519fe84609d07471ac5333b90281b02692ea18
Michael Dowling <mtdowling@gmail.com>
2014-07-19 17:28:24 -0700

    Cleaning up and adding more comments

--- src/jmespath/args.clj
+++ src/jmespath/args.clj
@@ -15,62 +15,55 @@
     (number? subject)           "number"
     (function? subject)         "expression"))
 
-(defn- get-expected-str [validator]
-  (get (meta validator) :validation))
-
 (defn- valid-type?
   "Determines if the provided argument matches the provided type"
   [expected arg]
-  (cond
-    (= expected "any") true
-    :else (= expected (gettype arg))))
+  (or (= expected "any") ; "any" matches any given argument type
+      (= expected (gettype arg))))
 
-(defn- invalid-arity! [fn-name args expected variadic]
+(defn- invalid-arity [fname args expected variadic]
   "Throws the correct exception for when an invalid argument arity"
   (let [expected-str (if variadic (str expected " or more") expected)]
-    (throw (Exception. (str fn-name " expects " expected-str " arguments. "
+    (throw (Exception. (str fname " expects " expected-str " arguments. "
                           (count args) " arguments were provided.")))))
 
-(defn- invalid-type! [fn-name pos expected actual arg-type]
+(defn- invalid-type [fname pos expected actual]
   "Throws an exception for when an invalid type is encountered"
-  (let [expected-str (get-expected-str expected)]
-    (throw (Exception. (str "Invalid " arg-type " provided to argument "
-                          (+ 1 pos) " of " fn-name ". Expected "
+  (let [expected-str (get (meta expected) :validation)]
+    (throw (Exception. (str "Invalid argument provided to argument "
+                          (+ 1 pos) " of " fname ". Expected "
                           expected-str ", got `" actual "`.")))))
 
-(defn- invalid-positional! [fn-name pos expected actual]
-  (invalid-type! fn-name pos expected actual "argument"))
-
-(defn- invalid-variadic! [fn-name pos expected actual]
-  (invalid-type! fn-name pos expected actual "variadic argument"))
-
-(defn- get-positioned-parameter [fn-name positional variadic pos args]
+(defn- get-positioned-parameter [fname positional variadic pos args]
+  "Gets a positional parameter from a list of parameters. If a postional
+   parameter does not exist at the given pos, then a variadic parameter
+   is returned if available, or an exeception is thrown if it is not
+   available."
   (let [p (or (get positional pos) variadic)]
-    (or p (invalid-arity! fn-name args (count positional) variadic))))
+    (or p (invalid-arity fname args (count positional) variadic))))
 
-(defn- validate-arg [fn-name positional variadic args pos]
-  "Validates a single argument of a function"
-  (let [p (get-positioned-parameter fn-name positional variadic pos args),
+(defn- validate-arg [fname positional variadic args pos]
+  "Validates a single argument of a function by ensuring the correct function
+   arity and that each argument type matches the expected type."
+  (let [p (get-positioned-parameter fname positional variadic pos args),
         a (get args pos)]
-    (cond
-      (p a) a
-      (contains? positional pos) (invalid-positional! fn-name pos p a)
-      :else (invalid-variadic! fn-name pos p a))))
+    (if (p a) a (invalid-type fname pos p a))))
 
 (defn arg-type [expected]
-  "Returns a function that validates a single type"
+  "Returns a function that checks if an argument is valid based on type"
   (with-meta
     (fn [arg] (valid-type? expected arg))
     {:validation expected}))
 
 (defn arg-alts [& alts]
-  "Returns a function that validates one or more types"
+  "Returns a function that checks if an argument is of one or more types"
   (with-meta
     (fn [arg] (some #(= % (gettype arg)) alts))
     {:validation (join " or " alts)}))
 
 (defn arg-seq [& types]
-  "Returns a function that ensures a collection uses a consistent type"
+  "Returns a function that ensures an argument collection uses a
+   consistent type"
   (with-meta
     (fn [arg]
       (and (= (gettype arg) "array")
@@ -82,11 +75,11 @@
 
 (defn validate-fn
   "Validates the arguments of a function"
-  [{fn-name :name
+  [{fname :name
     positional :positional
     variadic :variadic
     args :args}]
   (let [positional (or positional [])]
     (map
-      #(validate-arg fn-name positional variadic args %)
+      #(validate-arg fname positional variadic args %)
       (range (max (count args) (count positional))))))
