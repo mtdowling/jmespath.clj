68da814d419c3bf61bf1340be4907e26213f8138
Michael Dowling <mtdowling@gmail.com>
2014-07-20 15:21:34 -0700

    Adding avg(), ability to get from hash as key or keyword.

--- src/jmespath/functions.clj
+++ src/jmespath/functions.clj
@@ -5,23 +5,34 @@
 
 (defmulti invoke (fn [fname _] fname))
 
+(defmethod invoke "abs" [fname args]
+  (let [args (validate-fn {:name fname
+                           :positional [(arg-type "number")]
+                           :args (vec args)})]
+    (max (nth args 0) (- (nth args 0)))))
+
 (defmethod invoke "type" [fname args]
-  (let [args (validate-fn
-    {:name fname
-     :positional [(arg-type "any")]
-     :args (vec args)})]
+  (let [args (validate-fn {:name fname
+                           :positional [(arg-type "any")]
+                           :args (vec args)})]
     (gettype (nth args 0))))
 
 (defmethod invoke "not_null" [fname args]
-  (let [args (validate-fn
-    {:name "not_null"
-     :variadic (arg-type "any")
-     :args (vec args)})]
+  (let [args (validate-fn {:name "not_null"
+                           :variadic (arg-type "any")
+                           :args (vec args)})]
     (first (filter #(not= % nil) args))))
 
 (defmethod invoke "length" [fname args]
-  (let [args (validate-fn
-    {:name fname
-     :positional [(arg-alts "string" "array" "object")]
-     :args (vec args)})]
+  (let [args (validate-fn {:name fname
+                           :positional [(arg-alts "string" "array" "object")]
+                           :args (vec args)})]
     (count (nth args 0))))
+
+(defmethod invoke "avg" [fname args]
+  (let [args (validate-fn {:name fname
+                           :positional [(arg-seq "number")]
+                           :args (vec args)})]
+    ; Creates an average of a list of numbers by dividing sum by count
+    (/ (reduce + (nth args 0))
+       (count (nth args 0)))))
--- src/jmespath/tree.clj
+++ src/jmespath/tree.clj
@@ -5,17 +5,28 @@
 (defmulti visit (fn [ast _] (first ast)))
 
 (defmethod visit :identifier [ast data]
-  (when (map? data) (get data (get ast 1))))
+  "Visits an identifier node, returning either the string
+   referenced by the node, or a keyword referenced by the
+   node (e.g., 'a' or :a). If the argument is not a map,
+   then this method return nil."
+  (when (map? data)
+    (let [n (get ast 1)]
+      (or (data n) (data (keyword n))))))
 
 (defmethod visit :index [ast data]
+  "Returns the nth value of a sequence, or nil"
   (when (or (list? data) (vector? data))
     (get data (get ast 1))))
 
 (defmethod visit :sub-expr [ast data]
+  "Returns the value of the right expression passed into the
+   left expression."
   (visit (get ast 2)
          (visit (get ast 1) data)))
 
 (defmethod visit :pipe-expr [ast data]
+  "Returns the value of the right expression passed into the
+   left expression."
   (visit (get ast 2)
          (visit (get ast 1) data)))
 
@@ -32,6 +43,7 @@
       (= type :negation) (not (visit value data)))))
 
 (defmethod visit :binary-condition [ast data]
+  "Returns the result of a binary condition"
   (let [lhs (get ast 1)
         type (get-in ast [2 1])
         rhs (get ast 3)]
@@ -78,6 +90,7 @@
           (visit rexp item))))))
 
 (defmethod visit :multi-select-hash [ast data]
+  "Creates an array-map based on a list of key-value pair expressions"
   (apply
     array-map
     (flatten
@@ -88,11 +101,13 @@
         (rest ast)))))
 
 (defmethod visit :multi-select-list [ast data]
+  "Creates a vector based on a list of expressions"
   (map
     (fn [node] (visit node data))
     (rest ast)))
 
 (defmethod visit :function-expr [ast data]
+  "Invokes a function with a list of arguments"
   (invoke
     (get-in ast [1 1])
     (map (fn [node]
